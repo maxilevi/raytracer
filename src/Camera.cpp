71ee0f3d41f4c94dbea8b2c59bf04c9e6f11389b
Maximiliano Levi <maxilevi77@gmail.com>
2021-02-22 02:48:23 -0300

    create camera class

---
+++ src/Camera.cpp
@@ -0,0 +1,5 @@
+/*
+ * Created by Maximiliano Levi on 21/02/2021.
+ */
+
+#include "Camera.h"
---
+++ src/Camera.h
@@ -0,0 +1,14 @@
+/*
+ * Created by Maximiliano Levi on 21/02/2021.
+ */
+
+#ifndef RAYTRACER_CAMERA_H
+#define RAYTRACER_CAMERA_H
+
+
+class Camera {
+
+};
+
+
+#endif //RAYTRACER_CAMERA_H
