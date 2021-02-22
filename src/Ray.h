e8d673382e96df314ad210003aafcfdaaeb6da69
Maximiliano Levi <maxilevi77@gmail.com>
2021-02-22 02:48:47 -0300

    define a ray

--- src/Ray.h
+++ src/Ray.h
@@ -6,7 +6,19 @@
 #define RAYTRACER_RAY_H
 
 
+#include "Vector3.h"
+
 class Ray {
+public:
+    Ray();
+    Ray(const Vector3& origin, const Vector3& direction) : origin(origin), direction(direction) {};
+    inline Vector3 Origin() const { return this->origin; }
+    inline Vector3 Direction() const { return this->direction; }
+    inline Vector3 Point(float t) const { return this->origin + this->direction * t; }
+
+private:
+    Vector3 origin;
+    Vector3 direction;
 
 };
 
