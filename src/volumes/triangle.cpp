416a51e30fcbb8a87d58bea0bba3ea0599870525
Maximiliano Levi <maxilevi77@gmail.com>
2021-02-25 21:13:18 -0300

    add volumes folder

---
+++ src/volumes/triangle.cpp
@@ -0,0 +1,5 @@
+/*
+ * Created by Maximiliano Levi on 25/02/2021.
+ */
+
+#include "triangle.h"
---
+++ src/volumes/triangle.h
@@ -0,0 +1,14 @@
+/*
+ * Created by Maximiliano Levi on 25/02/2021.
+ */
+
+#ifndef RAYTRACER_TRIANGLE_H
+#define RAYTRACER_TRIANGLE_H
+
+
+class triangle {
+
+};
+
+
+#endif //RAYTRACER_TRIANGLE_H
