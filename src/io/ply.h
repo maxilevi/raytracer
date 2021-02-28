7e99c1b34b2740d4590ed2316ba3702ce9ec5359
Maximiliano Levi <maxilevi77@gmail.com>
2021-02-28 19:47:04 -0300

    add file for loading plys

---
+++ src/io/ply.h
@@ -0,0 +1,17 @@
+/*
+ * Created by Maximiliano Levi on 28/02/2021.
+ */
+
+#ifndef RAYTRACER_PLY_H
+#define RAYTRACER_PLY_H
+
+#include <string>
+#include "../volumes/triangle.h"
+
+std::unique_ptr<Triangle[]> LoadPLY(const std::string& path)
+{
+    
+}
+
+
+#endif //RAYTRACER_PLY_H
