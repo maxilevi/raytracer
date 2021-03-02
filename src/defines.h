b068175252f2544f67793b4725e9c75dd76d558e
Maximiliano Levi <maxilevi77@gmail.com>
2021-03-01 23:28:16 -0300

    add cuda define

--- src/defines.h
+++ src/defines.h
@@ -5,4 +5,7 @@
 #ifndef RAYTRACER_DEFINES_H
 #define RAYTRACER_DEFINES_H
 
+/* If we should use CUDA. If false the program uses C++17 std::execution::par_seq */
+#define USE_CUDA 0
+
 #endif //RAYTRACER_DEFINES_H
