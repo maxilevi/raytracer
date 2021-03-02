045470a4b2939b9e47253b6b0114ca946882e669
Maximiliano Levi <maxilevi77@gmail.com>
2021-03-02 20:19:07 -0300

    use nodiscard

--- src/volumes/sphere.h
+++ src/volumes/sphere.h
@@ -6,16 +6,17 @@
 #define RAYTRACER_SPHERE_H
 
 
-#include "../volume.h"
+#include "volume.h"
 
 class Sphere : public Volume  {
 public:
     Sphere(Vector3 center, double radius) : center_(center), radius_(radius) {}
     bool Hit(const Ray& ray, double t_min, double t_max, HitResult& record) const override;
+    bool BoundingBox(AABB& bounding_box) const override;
 
     /* Accessors and mutators */
-    inline double Radius() const { return radius_; }
-    inline Vector3 Center() const { return center_; }
+    [[nodiscard]] inline double Radius() const { return radius_; }
+    [[nodiscard]] inline const Vector3& Center() const { return center_; }
 
 private:
     bool IsValidHit(const Ray &ray, double t, double t_min, double t_max, HitResult& record) const;
