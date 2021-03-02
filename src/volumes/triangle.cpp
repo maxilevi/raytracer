21f5160ad2beb777b1214dd426c29a5eaa388b0e
Maximiliano Levi <maxilevi77@gmail.com>
2021-03-01 23:28:16 -0300

    finish triangle intersection code

--- src/volumes/triangle.cpp
+++ src/volumes/triangle.cpp
@@ -10,7 +10,7 @@
  *
  * https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
  * */
-bool Triangle::Intersects(const Ray &ray, double t_min, double t_max, double &t) const
+bool Triangle::Intersects(const Ray &ray, double &t) const
 {
     double epsilon = std::numeric_limits<double>::epsilon();
     auto edge1 = v_[1] - v_[0];
@@ -41,5 +41,31 @@ bool Triangle::Intersects(const Ray &ray, double t_min, double t_max, double &t)
 
 bool Triangle::Hit(const Ray &ray, double t_min, double t_max, HitResult &record) const
 {
-    return false;
+    double t;
+    if (!Intersects(ray, t)) return false;
+    if (t >= t_max || t <= t_min) return false;
+    record.t = t;
+    record.Point = ray.Point(record.t);
+    // TODO: Interpolate normals with barycentric coordinates
+    record.Normal = n_[0];
+    return true;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Triangle& triangle)
+{
+    stream << "(" << triangle.v_[0] << ", " << triangle.v_[1] << ", "  << triangle.v_[2] << ", ";
+    stream << triangle.n_[0] << ", " << triangle.n_[1] << ", "  << triangle.n_[2] << ")";
+    return stream;
+}
+
+void Triangle::Translate(Vector3 offset)
+{
+    for(auto & i : v_)
+        i += offset;
+}
+
+void Triangle::Scale(Vector3 scale)
+{
+    for(auto & i : v_)
+        i *= scale;
 }
--- src/volumes/triangle.h
+++ src/volumes/triangle.h
@@ -12,17 +12,40 @@
 
 class Triangle : public Volume {
 public:
+    Triangle() {};
     Triangle(Vector3 v0, Vector3 v1, Vector3 v2)
     {
         v_[0] = v0;
         v_[1] = v1;
         v_[2] = v2;
+
+        auto normal = Vector3::Cross(v1 - v0, v2 - v0);
+        n_[0] = normal;
+        n_[1] = normal;
+        n_[2] = normal;
     };
+
+    Triangle(Vector3 v0, Vector3 v1, Vector3 v2, Vector3 n0, Vector3 n1, Vector3 n2)
+    {
+        v_[0] = v0;
+        v_[1] = v1;
+        v_[2] = v2;
+
+        n_[0] = n0;
+        n_[1] = n1;
+        n_[2] = n2;
+    };
+
     bool Hit(const Ray& ray, double t_min, double t_max, HitResult& record) const override;
+    void Translate(Vector3 offset);
+    void Scale(Vector3 scale);
+
+    friend std::ostream& operator<<(std::ostream& stream, const Triangle& triangle);
 
 private:
-    bool Intersects(const Ray& ray, double t_min, double t_max, double& t) const;
+    bool Intersects(const Ray& ray, double& t) const;
     Vector3 v_[3];
+    Vector3 n_[3];
 };
 
 
