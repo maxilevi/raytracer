20a0a76c72f3b0041846442f34a6ba752bdcb731
Maximiliano Levi <maxilevi77@gmail.com>
2021-03-03 01:36:36 -0300

    add triangle list for representing modesl

---
+++ src/volumes/triangle_list.cpp
@@ -0,0 +1,4 @@
+/*
+ * Created by Maximiliano Levi on 03/03/2021.
+ */
+
---
+++ src/volumes/triangle_list.h
@@ -0,0 +1,14 @@
+/*
+ * Created by Maximiliano Levi on 03/03/2021.
+ */
+
+#ifndef RAYTRACER_TRIANGLE_LIST_H
+#define RAYTRACER_TRIANGLE_LIST_H
+
+
+class triangle_list {
+
+};
+
+
+#endif //RAYTRACER_TRIANGLE_LIST_H
