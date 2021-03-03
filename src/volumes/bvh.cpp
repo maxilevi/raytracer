369404246e6f107655fc2a3c20aee69fd33a7d5f
Maximiliano Levi <maxilevi77@gmail.com>
2021-03-03 01:37:07 -0300

    base bvh code

--- src/volumes/bvh.cpp
+++ src/volumes/bvh.cpp
@@ -3,3 +3,25 @@
  */
 
 #include "bvh.h"
+
+bool Bvh::Hit(const Ray &ray, double t_min, double t_max, HitResult &record) const
+{
+    if (!this->box_.Hit(ray, t_min, t_max))
+        return false;
+
+    bool hit_left = this->left_->Hit(ray, t_min, t_max, record);
+    bool hit_right = this->right_->Hit(ray, t_min, hit_left ? record.t : t_max, record);
+
+    return hit_left || hit_right;
+}
+
+bool Bvh::BoundingBox(AABB &bounding_box) const
+{
+    bounding_box = this->box_;
+    return true;
+}
+
+Bvh::Bvh(const Scene &scene)
+{
+
+}
--- src/volumes/bvh.h
+++ src/volumes/bvh.h
@@ -6,8 +6,21 @@
 #define RAYTRACER_BVH_H
 
 
-class bvh {
+#include "volume.h"
+#include "../scene.h"
 
+class Bvh : public Volume {
+public:
+    Bvh() = default;
+    Bvh(const Scene& scene);
+
+    bool Hit(const Ray& ray, double t_min, double t_max, HitResult& record) const override;
+    bool BoundingBox(AABB& bounding_box) const override;
+
+private:
+    AABB box_;
+    Volume* left_;
+    Volume* right_;
 };
 
 
