83ac1cce6521b05519263ec5078e02b56415eb2d
Maximiliano Levi <maxilevi77@gmail.com>
2021-03-03 01:37:07 -0300

    make triangle list a collection of tris

--- src/volumes/triangle_list.h
+++ src/volumes/triangle_list.h
@@ -6,8 +6,23 @@
 #define RAYTRACER_TRIANGLE_LIST_H
 
 
-class triangle_list {
+#include "volume.h"
+#include "triangle.h"
 
+class TriangleList : public Volume {
+public:
+    TriangleList(std::unique_ptr<Triangle[]> triangles, uint32_t count);
+
+    bool Hit(const Ray& ray, double t_min, double t_max, HitResult& record) const override;
+    bool BoundingBox(AABB& bounding_box) const override;
+    void Translate(Vector3 offset);
+    void Scale(Vector3 scale);
+
+    [[nodiscard]] inline uint32_t Size() const { return count_; }
+
+private:
+    std::unique_ptr<Triangle[]> triangles_;
+    uint32_t count_;
 };
 
 
